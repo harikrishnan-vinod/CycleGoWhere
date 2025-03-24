a4e993f08808039c03a6cd4faf8496a0ffcaf153
enjiaaaa <161003075+enjiaaaa@users.noreply.github.com>
2025-03-24 18:38:12 +0800

    change username, email, password

--- .src/frontend/src/components/ChangePassword.tsx
+++ .src/frontend/src/components/ChangePassword.tsx
@@ -1,11 +1,31 @@
+import { useNavigate } from "react-router-dom";
 import "../components-css/changePassword.css";
+import { useState } from "react";
+
 
 function changePassword() {
     return <div>
+
         <div>
-            <h1>Change Password</h1>
+            <header>CHANGE PASSWORD</header>
         </div>
-    </div>;
+        <div>
+            <form>
+                <ul className="change-password">
+                    <li>
+                        <input type="text" placeholder="New Password" className="new-password" required />
+                    </li>
+                    <li>
+                        <input type="text" placeholder="Confirm New Password" className="new-password" required />
+                    </li>
+                    <button type="submit" className="submit-btn">Change Password</button>
+                    <button onClick={() => navigate("/Settings")} type="button" className="cancel-btn">
+                        Cancel
+                    </button>
+                </ul>
+            </form>
+        </div>
+    </div >
 }
 
 export default changePassword;
--- .src/frontend/src/components/ChangeUsername.tsx
+++ .src/frontend/src/components/ChangeUsername.tsx
@@ -1,11 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import "../components-css/changeUsername.css";
 
+
 function changeUsername() {
+    const navigate = useNavigate();
+
     return <div>
+
         <div>
-            <h1>Change Username</h1>
+            <header>CHANGE USERNAME</header>
         </div>
-    </div>;
+        <div>
+            <form>
+                <ul className="change-username">
+                    <li>
+                        <input type="text" placeholder="New Username" className="new-username" required />
+                    </li>
+                    <li>
+                        <input type="text" placeholder="Confirm New Username" className="new-username" required />
+                    </li>
+                    <button type="submit" className="submit-btn">Change Username</button>
+                    <button onClick={() => navigate("/Settings")} type="button" className="cancel-btn">
+                        Cancel
+                    </button>
+                </ul>
+            </form>
+        </div>
+    </div >
 }
 
 export default changeUsername;
