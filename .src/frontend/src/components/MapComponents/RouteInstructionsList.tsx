17ad109e7f1596906253f5741f8a2673f67aa983
eyyt2309 <ethan97590773@gmail.com>
2025-03-26 13:38:03 +0800

    fixes

--- .src/frontend/src/components/MapComponents/RouteInstructionsList.tsx
+++ .src/frontend/src/components/MapComponents/RouteInstructionsList.tsx
@@ -18,7 +18,7 @@ const RouteInstructionList: React.FC<RouteInstructionListProps> = ({
       <ol className="route-instruction-list">
         {routeInstructions.map((instruction, index) => (
           <li key={index} className="route-instruction-item">
-            <strong>{instruction[0]}</strong>: {instruction[9]}
+            <strong>{instruction[9]}</strong>
             <div className="instruction-meta">
               {instruction[1] && <div>Road: {instruction[1]}</div>}
               Distance: {instruction[5]}
